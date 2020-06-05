cd91a0d4a5cea34d5c0a3c3bece7dff3125db4ec
Alexander <al.deimosfrontend@gmail.com>
2020-06-05 19:21:17 +0300

    Updates

---
+++ precache-manifest.d294d576933e4e912ca06407f6a1046c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "381ffddd916a5229c3737bf07bb54cae",
+    "url": "/moviedb/index.html"
+  },
+  {
+    "revision": "957114f237aaa1afeb57",
+    "url": "/moviedb/static/css/2.5741eab2.chunk.css"
+  },
+  {
+    "revision": "e241da0d57f0c1133d15",
+    "url": "/moviedb/static/css/main.fa2cf59f.chunk.css"
+  },
+  {
+    "revision": "957114f237aaa1afeb57",
+    "url": "/moviedb/static/js/2.d9455b97.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/moviedb/static/js/2.d9455b97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e241da0d57f0c1133d15",
+    "url": "/moviedb/static/js/main.8de70bc8.chunk.js"
+  },
+  {
+    "revision": "3ef2c0b5513e19a297e2",
+    "url": "/moviedb/static/js/runtime-main.3878a2e8.js"
+  },
+  {
+    "revision": "05e37c736aefcb18c783fa1df83de83f",
+    "url": "/moviedb/static/media/favorite_off.05e37c73.svg"
+  },
+  {
+    "revision": "1f43ea1cab2bc6a84649b597819cd213",
+    "url": "/moviedb/static/media/favorite_on.1f43ea1c.svg"
+  },
+  {
+    "revision": "56fd61eec148690af48f876dfd5c8718",
+    "url": "/moviedb/static/media/logo.56fd61ee.svg"
+  },
+  {
+    "revision": "21a1bdaab497702da0bee79802145fc6",
+    "url": "/moviedb/static/media/male.21a1bdaa.svg"
+  }
+]);
